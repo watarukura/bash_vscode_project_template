7378a23867811d76c73d1649f2b010d0d0aeafe2
watarukura <what.r.j150@gmail.com>
2020-08-09 09:54:13 +0900

    fix: shellcheck SC2145

--- hello.bash
+++ hello.bash
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 function err() {
-    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z') line:${BASH_LINENO[0]}]: $@" >&2
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z') line:${BASH_LINENO[0]}]: $*" >&2
     exit 1
 }
 
