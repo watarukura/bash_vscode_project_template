9dc940bb5583b2b825759bc52c0bb1add7f472ff
wataru-kurashima <wataru-kurashima@hands-lab.com>
2020-09-04 13:13:37 +0900

    feat: trap ERR/EXIT

--- functions.bash
+++ functions.bash
@@ -1,4 +1,18 @@
-function err() {
+tmp_dir=$(mktemp -d)
+log_dir=./log
+
+function error_exit() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z') line:${BASH_LINENO[0]}]: $*" >&2
+    mv "${tmp_dir}" "${log_dir}"/
     exit 1
 }
+
+function normal_exit() {
+    exit 0
+}
+
+# エラー終了
+trap error_exit ERR
+
+# 正常終了
+trap normal_exit EXIT
