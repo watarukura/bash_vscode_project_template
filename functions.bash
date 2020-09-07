2f09efc1ba7b4c3f91574e33a658855af3fbb306
watarukura <what.r.j150@gmail.com>
2020-09-07 18:31:31 +0900

    fix: shellcheck

--- functions.bash
+++ functions.bash
@@ -1,14 +1,15 @@
 function error_exit() {
     _log_level="ERROR" _log_line=${BASH_LINENO[0]} logging "$@"
     mv "${tmp_dir}" "${log_dir}"
-    cd "${log_dir}" && mv $(basename ${tmp_dir}) $(basename ${0}).${now}.$$ || exit 1
+    cd "${log_dir}" && mv $(basename "${tmp_dir}") "$(basename ${0}).${now}.$$"
+    exit 1
 }
 
 function logging() {
     declare -r date=$(date +${now_log})
     declare -r line=${_log_line:-${BASH_LINENO[0]}}
     declare -r level=${_log_level:-"INFO"}
-    declare -r message=$(echo $* | sed -e 's/"/\\"/g')
+    declare -r message=$(echo "$@" | sed -e 's/"/\\"/g')
     cat <<EOF | tee -a ${log_file}
 {"date":"${date}","line":${line},"level":"${level}","message":"${message}"}
 EOF
