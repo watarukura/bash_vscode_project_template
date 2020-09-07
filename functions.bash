546d5303e846e76189fb78c2050dcb97ce47b774
watarukura <what.r.j150@gmail.com>
2020-09-07 18:24:09 +0900

    fix: shellcheck

--- functions.bash
+++ functions.bash
@@ -1,9 +1,7 @@
 function error_exit() {
-    _log_level=ERROR _log_line=${BASH_LINENO[0]} logging $*
+    _log_level="ERROR" _log_line=${BASH_LINENO[0]} logging "$@"
     mv "${tmp_dir}" "${log_dir}"
-    cd ${log_dir}
-    mv $(basename ${tmp_dir}) $(basename ${0}).${now}.$$
-    exit 1
+    cd ${log_dir} && mv $(basename ${tmp_dir}) $(basename ${0}).${now}.$$ || exit 1
 }
 
 function logging() {
